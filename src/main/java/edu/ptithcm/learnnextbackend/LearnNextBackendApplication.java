7df33a1c65d1d535d5631d5de66a85858efaac0e
Ho Duy Nhat <hdnhat2005@gmail.com>
2026-05-01 16:00:29 +0700

    feature(auth): add annotation

--- src/main/java/edu/ptithcm/learnnextbackend/LearnNextBackendApplication.java
+++ src/main/java/edu/ptithcm/learnnextbackend/LearnNextBackendApplication.java
@@ -2,8 +2,10 @@ package edu.ptithcm.learnnextbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class LearnNextBackendApplication {
 
     public static void main(String[] args) {
