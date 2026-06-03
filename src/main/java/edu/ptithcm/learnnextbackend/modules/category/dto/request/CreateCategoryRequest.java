73bb8ec2ee0dc49e4ee2212be201be6c6b627b23
Ho Duy Nhat <hdnhat2005@gmail.com>
2026-06-03 22:15:48 +0700

    feature(search): add request/response category

---
+++ src/main/java/edu/ptithcm/learnnextbackend/modules/category/dto/request/CreateCategoryRequest.java
@@ -0,0 +1,13 @@
+package edu.ptithcm.learnnextbackend.modules.category.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class CreateCategoryRequest {
+
+    @NotBlank(message = "Category name is required")
+    private String name;
+
+    private String description;
+}
---
+++ src/main/java/edu/ptithcm/learnnextbackend/modules/category/dto/response/CategoryResponse.java
@@ -0,0 +1,18 @@
+package edu.ptithcm.learnnextbackend.modules.category.dto.response;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+@Builder
+public class CategoryResponse {
+
+    private UUID id;
+    private String name;
+    private String slug;
+    private String description;
+    private boolean active;
+
+}
