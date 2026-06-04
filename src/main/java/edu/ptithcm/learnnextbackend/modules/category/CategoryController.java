57804c63e642e7644de15aa48e5aca1c33f0d2a5
Ho Duy Nhat <hdnhat2005@gmail.com>
2026-06-04 08:24:32 +0700

    feature(search): API controller create, getAll, getById

--- src/main/java/edu/ptithcm/learnnextbackend/modules/category/CategoryController.java
+++ src/main/java/edu/ptithcm/learnnextbackend/modules/category/CategoryController.java
@@ -1,5 +1,34 @@
 package edu.ptithcm.learnnextbackend.modules.category;
 
+import edu.ptithcm.learnnextbackend.modules.category.dto.request.CreateCategoryRequest;
+import edu.ptithcm.learnnextbackend.modules.category.dto.response.CategoryResponse;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/categories")
 public class CategoryController {
-    
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @PostMapping
+    public ResponseEntity<CategoryResponse> create(@RequestBody @Valid CreateCategoryRequest req) {
+        return ResponseEntity.ok(categoryService.create(req));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CategoryResponse>> getAll() {
+        return ResponseEntity.ok(categoryService.getAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CategoryResponse> getById(@PathVariable UUID id) {
+        return ResponseEntity.ok(categoryService.getById(id));
+    }
 }
